ceb4109a424615fbb864091b9500a36ab183bc37
invitado07 <invitado07@loginfc.lan>
2018-04-29 11:55:29 -0700

    Solucion Pistas De Azul

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 
 using namespace std;
 struct instrucciones{
@@ -24,38 +23,10 @@ int main()
 
         }
         for(int i = 0; i < n; i++)
-=======
-#include <queue>
-using namespace std;
-struct siguientes{
-char actual;
-int fila;
-int col;
-};
-int main()
-{
-
-    int n;
-    cin >> n;
-    siguientes matriz[n][n];
-    bool visitados[n][n];
-    queue <siguientes> arbol;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-        {
-
-
-            cin >> matriz[i][j].actual;
-            matriz[i][j].fila = i;
-            matriz[i][j].col = j;
-        }
- for(int i = 0; i < n; i++)
->>>>>>> 81e823d8bf7e784e4ad70aaf98518d70bf09a57c
         for(int j = 0; j < n; j++)
         {
             visitados[i][j] = false;
         }
-<<<<<<< HEAD
 
           for(int i = 0; i < n; i++)
             for(int j = 0; j < n; j++)
@@ -73,7 +44,7 @@ int main()
               if(mapa[i][j].fila == FF && mapa[i][j].columna == FC)
               {
                   contador++;
-                  cout << i << " " << j << endl;
+
               }
                for(int l = 0; l < n; l++)
                   for(int t = 0; t < n; t++)
@@ -90,48 +61,5 @@ int main()
 
      cout << contador;
 
-=======
-    arbol.push(matriz[0][0]);
-
-    while(!arbol.empty())
-    {
-        siguientes nodoAct = arbol.front();
-
-
-
-        cout << arbol.front().actual << " ";
-
-        visitados[nodoAct.fila][nodoAct.col] = true;
-        arbol.pop();
-         if((visitados[nodoAct.fila + 1][nodoAct.col] == true  && visitados[nodoAct.fila][nodoAct.col + 1] == true) || (nodoAct.fila == n-1 && nodoAct.col == n-1))
-        {
-
-            cout << "no tiene conexiones";
-
-        }
-        if(visitados[nodoAct.fila + 1][nodoAct.col] == false && nodoAct.fila < n-1)
-        {
-            arbol.push(matriz[nodoAct.fila + 1][nodoAct.col]);
-            cout << "conecta con " << matriz[nodoAct.fila + 1][nodoAct.col].actual << " ";
-             visitados[nodoAct.fila + 1][nodoAct.col] = true;
-        }
-
-         else if(visitados[nodoAct.fila][nodoAct.col + 1] == false && nodoAct.col < n-1)
-        {
-            arbol.push(matriz[nodoAct.fila][nodoAct.col + 1]);
-             cout << "conecta con " << matriz[nodoAct.fila][nodoAct.col + 1].actual << " ";
-             visitados[nodoAct.fila][nodoAct.col + 1] = true;
-        }
-        if(visitados[nodoAct.fila][nodoAct.col + 1] == false && nodoAct.col < n-1)
-        {
-            arbol.push(matriz[nodoAct.fila][nodoAct.col + 1]);
-             cout << "y con " << matriz[nodoAct.fila][nodoAct.col + 1].actual << " ";
-             visitados[nodoAct.fila][nodoAct.col + 1] = true;
-        }
-
-
-          cout << endl;
-    }
->>>>>>> 81e823d8bf7e784e4ad70aaf98518d70bf09a57c
     return 0;
 }
